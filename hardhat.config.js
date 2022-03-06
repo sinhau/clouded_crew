c577dd141b0c576e04ce75b0e6676bfa7c0d80c4
Utkarsh Sinha <sinha.karsh@gmail.com>
2022-03-06 13:25:18 -0800

    gitignore, packages

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+
+const { RINKEBY_API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.1",
+  settings: {
+    optimizer: {
+      enabled: true,
+      run: 100000,
+    },
+  },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: RINKEBY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 150,
+    enabled: true,
+    coinmarketcap: "cb2bc4d1-c791-4b17-b6cb-3d9182deeebd",
+  },
+};
