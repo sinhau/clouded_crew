02bb226fc00a8c77a0925782d32dc01a2be60e86
Utkarsh Sinha <sinha.karsh@gmail.com>
2022-04-03 21:50:02 -0700

    removing old frontend

--- frontend/web3/ethdata.tsx
+++
@@ -1,8 +0,0 @@
-import { CHAINS } from "@lido-sdk/constants";
-
-export const supportedChainIds = [CHAINS.Mainnet, CHAINS.Rinkeby];
-
-export const rpc = {
-  [CHAINS.Mainnet]: "https://main-light.eth.linkpool.io",
-  [CHAINS.Rinkeby]: "https://eth-rinkeby.alchemyapi.io/v2/Tu1kbaSWQJAlqnmR1vdwfFWdWm-NqGYL"
-};
